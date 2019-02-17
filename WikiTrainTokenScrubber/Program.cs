8454cccce9fbdaef03af58991da185f475c86f11
BrettsSchmidt <brettsschmidt@gmail.com>
2019-02-16 19:18:14 -0500

    Updated Main() to turn wiki.train.tokens into a matrix of index locations.

--- WikiTrainTokenScrubber/Program.cs
+++ WikiTrainTokenScrubber/Program.cs
@@ -9,8 +9,8 @@ namespace WikiTrainTokenScrubber
 {
     class Program
     {
-        //1349980
-        static void Main(string[] args)
+
+        static void HashCharsMatrixes()
         {
             Random rand = new Random();
             var index = new List<string>();
@@ -23,8 +23,9 @@ namespace WikiTrainTokenScrubber
                     //Read to end of the stream.
                     while (!sr.EndOfStream)
                     {
-                        
+
                         var masterList = new List<KeywordPair>();
+
                         // Get the current line
                         var sline = sr.ReadLine();
                         // Make sure the line actually has words
@@ -86,64 +87,174 @@ namespace WikiTrainTokenScrubber
                 }
             }
 
-            
-            
-
-        }
-    }
-
-    class KeywordPair
-    {
-        public string First { get; set; }
-        public string Second { get; set; }
-        public KeywordPair(string first, string second)
-        {
-            First = first;
-            Second = second;
         }
 
-        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        static string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.?! ";
 
-        public static string GetOnlyChars(string s)
+        static string ConvertFloatListToString(List<float> listy)
         {
-            var newstring = "";
-            foreach(var c in s)
+            var result = "";
+            result += listy[0];
+            for (int i = 1; i < listy.Count; i++)
             {
-                if(alphabet.Contains(c) || alphabet.ToUpper().Contains(c))
+                result += " " + listy[i];
+            }
+
+            return result;
+
+        }
+
+
+        //1349980
+        static void Main(string[] args)
+        {
+            //Scrub into array based off word index.
+
+            Random rand = new Random();
+            var index = new List<string>();
+
+            //Load document
+            using (var sr = new StreamReader("C:\\Users\\brett\\Documents\\MATLAB\\wikitext-2\\wiki.train.tokens"))
+            {
+                using (var sw = new StreamWriter("C:\\Users\\brett\\Documents\\MATLAB\\wikitext-2\\wikiindextokens.txt"))
                 {
-                    newstring += c;
+                    //Read to end of the stream.
+                    while (!sr.EndOfStream)
+                    {
+
+
+                        // Get the current line
+                        var sline = sr.ReadLine();
+                        // Make sure the line actually has words and isn't a category
+                        if (sline != "" && sline != " " && !sline.Contains('='))
+                        {
+
+
+                            // Remove non-alphabetical characters or non-sentence ending chars
+                            var schars = sline;
+                            foreach (var c in schars)
+                            {
+                                if (!Alphabet.Contains(c) && c != ' ')
+                                {
+                                    sline = sline.Replace(c.ToString(), "");
+                                }
+                            }
+
+                            // Get the sentences
+                            var sentences = sline.Split('.', '?', '!');
+
+                            //Iterate through the sentences
+                            foreach (var sentence in sentences)
+                            {
+                                // Initalize array of floats (our 5x5 matrix represented in 1-dim)
+                                var array = new List<float>();
+                                // Create an array of words by splitting up the line by spaces.
+                                var splity = sentence.Split(' ');
+                                // Make sure its not an empty spot or a sentence with more than 25 words
+                                //      and iterate through the words.
+                                //      I also don't want the sentences with "unk"
+                                if (splity.Length > 1 && splity.Length < 26 && !splity.Contains("unk"))
+                                {
+                                    for (int i = 0; i < splity.Length; i++)
+                                    {
+                                        if (index.Exists(x => x == splity[i]))
+                                        {
+                                            array.Add(index.FindIndex(x => x == splity[i]) + 1);
+                                        }
+                                        else
+                                        {
+                                            index.Add(splity[i]);
+                                            array.Add(index.Count);
+                                        }
+                                    }
+                                    for (int i = splity.Length; i < 26; i++)
+                                    {
+                                        array.Add(0);
+                                    }
+                                    sw.WriteLine(ConvertFloatListToString(array));
+                                }
+
+
+
+                            }
+                        }
+
+
+
+
+                    }
+
+
+                }
+                using (var sw = new StreamWriter("C:\\Users\\brett\\Documents\\MATLAB\\wikitext-2\\wikiindex.txt"))
+                {
+                    for(int i = 0; i < index.Count; i++)
+                    {
+                        sw.WriteLine(i + " " + index[i]);
+                    }
                 }
             }
-            return newstring;
+
         }
 
-        public static int GetCharBin(char c, Random rand)
+
+    }
+}
+
+
+
+class KeywordPair
+{
+    public string First { get; set; }
+    public string Second { get; set; }
+    public KeywordPair(string first, string second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static string GetOnlyChars(string s)
+    {
+        var newstring = "";
+        foreach (var c in s)
         {
-            
-            if(alphabet.Contains(c))
+            if (alphabet.Contains(c) || alphabet.ToUpper().Contains(c))
             {
-                return ((alphabet.IndexOf(c) + 1) * 10) + rand.Next(0, 9);
-            }
-            else if(alphabet.ToUpper().Contains(c))
-            {
-                return ((alphabet.ToUpper().IndexOf(c) + 1) * 10) + rand.Next(0, 9) + 260;
-            }
-            else
-            {
-                return rand.Next(0, 9);
+                newstring += c;
             }
         }
+        return newstring;
+    }
 
-        public static string ConvertMatToString(int[] mat)
+    public static int GetCharBin(char c, Random rand)
+    {
+
+        if (alphabet.Contains(c))
         {
-            var s = "";
-            foreach(var inty in mat)
-            {
-                s += inty + " ";
-            }
-            s = s.Remove(s.Length - 1);
-            return s;
+            return ((alphabet.IndexOf(c) + 1) * 10) + rand.Next(0, 9);
+        }
+        else if (alphabet.ToUpper().Contains(c))
+        {
+            return ((alphabet.ToUpper().IndexOf(c) + 1) * 10) + rand.Next(0, 9) + 260;
+        }
+        else
+        {
+            return rand.Next(0, 9);
         }
     }
 
+    public static string ConvertMatToString(int[] mat)
+    {
+        var s = "";
+        foreach (var inty in mat)
+        {
+            s += inty + " ";
+        }
+        s = s.Remove(s.Length - 1);
+        return s;
+    }
 }
+
+
